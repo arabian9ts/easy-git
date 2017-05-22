d883e0c68a250568ec43d244f12fc9781ce27312
arabian9ts <nok14.tm59@gmail.com>
2017-05-22 11:39:00 +0900

    logger added

---
+++ eat/log.cpp
@@ -0,0 +1,9 @@
+//
+//  log.cpp
+//  easy-git
+//
+//  Created by arabian9ts on 2017/05/22.
+//  Copyright © 2017年 arabian9ts. All rights reserved.
+//
+
+#include "log.hpp"
---
+++ eat/log.hpp
@@ -0,0 +1,14 @@
+//
+//  log.hpp
+//  easy-git
+//
+//  Created by arabian9ts on 2017/05/22.
+//  Copyright © 2017年 arabian9ts. All rights reserved.
+//
+
+#ifndef log_hpp
+#define log_hpp
+
+#include <stdio.h>
+
+#endif /* log_hpp */
