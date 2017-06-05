2a42a4a7e69a113b975a7ca435bb10ce4d41241d
arabian9ts <nok14.tm59@gmail.com>
2017-06-05 10:52:12 +0900

    compress features declared

--- eat/compress.cpp
+++ eat/compress.cpp
@@ -7,3 +7,21 @@
 //
 
 #include "compress.hpp"
+
+/**
+ * 圧縮する文字列を指定したファイル名に圧縮
+ * @param tar : 圧縮する文字列
+ * @param named : 圧縮先のファイル名
+ */
+void cmp::compress(std::string tar, std::string named){
+    
+}
+
+/**
+ * 指定したファイル名のファイルを解答して、元の文字列を返却する
+ * @param named : 解答するファイル名
+ * @return : 解答した文字列
+ */
+std::string cmp::decompress(std::string named){
+    return "";
+}
--- eat/compress.hpp
+++ eat/compress.hpp
@@ -10,5 +10,16 @@
 #define compress_hpp
 
 #include <stdio.h>
+#include <string>
+
+namespace cmp {
+    
+    /** 圧縮する文字列を指定したファイル名に圧縮 */
+    void compress(std::string tar, std::string named);
+    
+    /** 指定したファイル名のファイルを解答して、元の文字列を返却する */
+    std::string decompress(std::string named);
+    
+}
 
 #endif /* compress_hpp */
