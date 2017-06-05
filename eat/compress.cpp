c06b28f3ee60c02a98dc2bb90c1eecb23f0a547b
arabian9ts <nok14.tm59@gmail.com>
2017-06-05 10:38:47 +0900

    compress c++ file added

---
+++ eat/compress.cpp
@@ -0,0 +1,9 @@
+//
+//  compress.cpp
+//  easy-git
+//
+//  Created by arabian9ts on 2017/06/05.
+//  Copyright © 2017年 arabian9ts. All rights reserved.
+//
+
+#include "compress.hpp"
---
+++ eat/compress.hpp
@@ -0,0 +1,14 @@
+//
+//  compress.hpp
+//  easy-git
+//
+//  Created by arabian9ts on 2017/06/05.
+//  Copyright © 2017年 arabian9ts. All rights reserved.
+//
+
+#ifndef compress_hpp
+#define compress_hpp
+
+#include <stdio.h>
+
+#endif /* compress_hpp */
