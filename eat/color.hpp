94fdcc0defeb3705bdac7503c306d826fbb39b55
arabian9ts <nok14.tm59@gmail.com>
2017-06-12 11:14:03 +0900

    color header added

---
+++ eat/color.hpp
@@ -0,0 +1,13 @@
+//
+//  color.hpp
+//  easy-git
+//
+//  Created by arabian9ts on 2017/06/12.
+//  Copyright © 2017年 arabian9ts. All rights reserved.
+//
+
+#ifndef color_hpp
+#define color_hpp
+
+
+#endif /* color_hpp */
